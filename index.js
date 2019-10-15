b9c364f3fc89a85f7cdb77246559117cd458106d
luoqiang <luoqiang314@gmail.com>
2019-10-15 09:13:50 +0800

    获取post get 请求参数

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 const http = require('http')
+const url = require('url')
+const querystring = require('querystring')
 
 const home = require('./pages/home.js')
 const list = require('./pages/list.js')
 const user = require('./pages/user.js')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    if (req.url === '/' || req.url === '/home') {
-        res.end(home)
-        return
-    }
-    if (req.url === '/list') {
-        res.end(list)
-        return
-    }
-    if (req.url === '/user') {
-        res.end(user)
-        return
-    }
+  // 定义公共变量，存储请求方法、路径、数据
+  const method = req.method
+  let path = ''
+  let param = {}
 
-    res.end(JSON.stringify('404------没有匹配到路由'))
+  //请求路径
+  const parsed = url.parse(req.url)
+  const pathname = parsed.pathname
+
+  // 判断请求方法为GET还是POST，区分处理数据
+  if (method === 'GET') {
+    const { pathname, query } = url.parse(req.url, true)
+
+    path = pathname
+    param = query
+  } else if (method === 'POST') {
+    path = req.url
+    let arr = []
+
+    req.on('data', (buffer) => {
+      arr.push(buffer)
+    })
+
+    req.on('end', () => {
+      let buffer = Buffer.concat(arr)
+      param = parseQueryStr(buffer.toString())
+    })
+  }
+
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+
+  const data = { method, param }
+
+  if (path === '/' || pathname === '/home') {
+    res.end(home + JSON.stringify(data))
+    return
+  }
+  if (path === '/list') {
+    res.end(list + JSON.stringify(data))
+    return
+  }
+  if (path === '/user') {
+    res.end(user + JSON.stringify(data))
+    return
+  }
+
+  res.end(JSON.stringify('404------没有匹配到路由'))
 })
 
 server.listen(8080, () => console.log('listen on 8080'))
+console.log('服务启动成功')
